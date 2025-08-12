c1b5a249027005640452d1fb4bbe9761779ca104
Deepak Paramanick <deepak.paramanick@AurionproSolutions.com>
2025-08-12 12:00:25 +0530

    Created POM pages for AssetDetails Page and CustomerDetails Page.

---
+++ POM/DoPages/DoIndividualPages/DoPersonalDetailsPage.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+
+namespace UDC.POM.DoPages.DoIndividualPages
+{
+    public class DoPersonalDetailsPage : BasePage
+    {
+        public DoPersonalDetailsPage(IWebDriver driver) : base(driver)
+        {
+        }
+        private IWebElement firstName => Find(By.XPath("//label[normalize-space()='First Name']/following-sibling::div//input"));
+
+    }
+}
---
+++ POM/DoPages/DoTrustPages/DoTrustDetailsPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace UDC.POM.DoPages.DoTrustPages
+{
+    public class DoTrustDetailsPage : BasePage
+    {
+        public DoTrustDetailsPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+    }
+}
